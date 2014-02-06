d80b28085c82a621a0dbc9c9cb6816f0f4f18ea9
Adam Tait <adam@adamtait.com>
2014-02-05 18:39:54 -0800

    Initial Commit

---
+++ pronounce/TCPAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TCPAppDelegate.h
+//  pronounce
+//
+//  Created by Adam Tait on 2/5/14.
+//  Copyright (c) 2014 Team Canada. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TCPAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ pronounce/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  pronounce
+//
+//  Created by Adam Tait on 2/5/14.
+//  Copyright (c) 2014 Team Canada. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TCPAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([TCPAppDelegate class]));
+    }
+}
