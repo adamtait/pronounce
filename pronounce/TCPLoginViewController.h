7b1c4a8537a3e48dcb0899545c8ae4721fa17aff
Adam Tait <adam@adamtait.com>
2014-02-11 14:37:54 -0800

    add basics for Facebook login with Parse; including FB & Parse frameworks, and LoginViewController

---
+++ pronounce/TCPLoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  TCPLoginViewController.h
+//  pronounce
+//
+//  Created by Adam Tait on 2/11/14.
+//  Copyright (c) 2014 Team Canada. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TCPLoginViewController : UIViewController
+
+@end
---
+++ pronounce/TCPLoginViewController.m
@@ -0,0 +1,38 @@
+//
+//  TCPLoginViewController.m
+//  pronounce
+//
+//  Created by Adam Tait on 2/11/14.
+//  Copyright (c) 2014 Team Canada. All rights reserved.
+//
+
+#import "TCPLoginViewController.h"
+
+@interface TCPLoginViewController ()
+
+@end
+
+@implementation TCPLoginViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
