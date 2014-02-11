d64e8283357c1987a81a1b0e7a7efa518dcc10d9
Jonathan Xu <jonathan@randomguava.com>
2014-02-11 12:53:21 -0800

    Profile view for mockup, screen shots

---
+++ pronounce/ViewControllers/TCPProfileViewController.h
@@ -0,0 +1,13 @@
+//
+//  TCPProfileViewController.h
+//  pronounce
+//
+//  Created by Jonathan Xu on 2/11/14.
+//  Copyright (c) 2014 Team Canada. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TCPProfileViewController : UIViewController
+
+@end
---
+++ pronounce/ViewControllers/TCPProfileViewController.m
@@ -0,0 +1,38 @@
+//
+//  TCPProfileViewController.m
+//  pronounce
+//
+//  Created by Jonathan Xu on 2/11/14.
+//  Copyright (c) 2014 Team Canada. All rights reserved.
+//
+
+#import "TCPProfileViewController.h"
+
+@interface TCPProfileViewController ()
+
+@end
+
+@implementation TCPProfileViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
