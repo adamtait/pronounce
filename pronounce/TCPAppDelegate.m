6732fe705851c5ef1f5bfbc4fa171e38b84d1bf8
Adam Tait <adam@adamtait.com>
2014-02-21 00:45:30 -0800

    [#65693410] add additional functions to handle URL returned from FB Oauth
    Story: https://www.pivotaltracker.com/story/show/65693410

--- pronounce/TCPAppDelegate.m
+++ pronounce/TCPAppDelegate.m
@@ -13,7 +13,7 @@
 #import <Parse/Parse.h>
 
 @interface TCPAppDelegate () <UITabBarControllerDelegate>
-@property (strong, nonatomic) UITabBarController *tabBar;
+    @property (strong, nonatomic) UITabBarController *tabBar;
 @end
 
 @implementation TCPAppDelegate
@@ -30,8 +30,8 @@
     self.window.backgroundColor = [UIColor whiteColor];
     [self.window makeKeyAndVisible];
     
-//    TCPLoginViewController *vc = [[TCPLoginViewController alloc] init];
-    self.window.rootViewController = self.tabBar;
+    TCPLoginViewController *vc = [[TCPLoginViewController alloc] init];
+    self.window.rootViewController = vc; //self.tabBar;
     
     return YES;
 }
@@ -85,14 +85,30 @@
     // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
 }
 
-- (void)applicationDidBecomeActive:(UIApplication *)application
-{
-    // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+// ****************************************************************************
+// App switching methods to support Facebook Single Sign-On.
+// ****************************************************************************
+- (BOOL)application:(UIApplication *)application openURL:(NSURL *)url sourceApplication:(NSString *)sourceApplication annotation:(id)annotation {
+    return [FBAppCall handleOpenURL:url
+                  sourceApplication:sourceApplication
+                        withSession:[PFFacebookUtils session]];
 }
 
-- (void)applicationWillTerminate:(UIApplication *)application
-{
-    // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+- (void)applicationDidBecomeActive:(UIApplication *)application {
+    /*
+     Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+     */
+    
+    [FBAppCall handleDidBecomeActiveWithSession:[PFFacebookUtils session]];
+}
+
+- (void)applicationWillTerminate:(UIApplication *)application {
+    /*
+     Called when the application is about to terminate.
+     Save data if appropriate.
+     See also applicationDidEnterBackground:.
+     */
+    [[PFFacebookUtils session] close];
 }
 
 @end
