2c8b94215b2b88689e80cf043dc2c38adeda63b6
Adam Tait <adam@adamtait.com>
2014-02-25 01:06:55 -0800

    [#66301628] add TCPColorFactory
    + a place to keep track of the color pallete used through the app
    
    Story: https://www.pivotaltracker.com/story/show/66301628

---
+++ pronounce/TCPColorFactory.h
@@ -0,0 +1,20 @@
+//
+//  TCPColorFactory.h
+//  pronounce
+//
+//  Created by Adam Tait on 2/25/14.
+//  Copyright (c) 2014 Team Canada. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TCPColorFactory : NSObject
+
+    + (UIColor *)blueColor;
+    + (UIColor *)purpleColor;
+    + (UIColor *)redColor;
+
+    + (UIColor *)whiteColor;
+    + (UIColor *)lightGrayColor;
+
+@end
