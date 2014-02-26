0053d7ec15fbd6494aabdd8219ba998720c27573
Adam Tait <adam@adamtait.com>
2014-02-26 01:49:04 -0800

    [#66118216] add AWS SDK pod + new API class
    Story: https://www.pivotaltracker.com/story/show/66118216

---
+++ pronounce/TCPAwsAPI.h
@@ -0,0 +1,13 @@
+//
+//  TCPAwsAPI.h
+//  pronounce
+//
+//  Created by Adam Tait on 2/25/14.
+//  Copyright (c) 2014 Team Canada. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TCPAwsAPI : NSObject
+
+@end
---
+++ pronounce/TCPAwsAPI.m
@@ -0,0 +1,13 @@
+//
+//  TCPAwsAPI.m
+//  pronounce
+//
+//  Created by Adam Tait on 2/25/14.
+//  Copyright (c) 2014 Team Canada. All rights reserved.
+//
+
+#import "TCPAwsAPI.h"
+
+@implementation TCPAwsAPI
+
+@end
