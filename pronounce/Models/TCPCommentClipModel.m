72a43cced119970ba9bf857afe9b9f5596a4ed0b
Adam Tait <adam@adamtait.com>
2014-02-26 01:51:37 -0800

    [#66118216] add audio data management methods to CommentClipModel
    + let's you create a new model with a file URL
    + later, call saveInBackground
    
    Story: https://www.pivotaltracker.com/story/show/66118216

--- pronounce/Models/TCPCommentClipModel.m
+++ pronounce/Models/TCPCommentClipModel.m
@@ -7,7 +7,42 @@
 //
 
 #import "TCPCommentClipModel.h"
+#import "TCPAwsAPI.h"
+
+@interface TCPCommentClipModel ()
+
+    // private properties
+    @property (nonatomic, strong) NSString *uniqueID;
+    @property (nonatomic, strong) NSURL *audioFileUrl;
+
+    // private class methods
+    + (NSString *)generateUUID;
+
+@end
 
 @implementation TCPCommentClipModel
+
++ (NSString *)generateUUID
+{
+    CFUUIDRef uuid = CFUUIDCreate(kCFAllocatorDefault);
+    return (__bridge_transfer NSString *)CFUUIDCreateString(kCFAllocatorDefault, uuid);
+}
+
+- (id)initWithAudioDataFileURL:(NSURL *)audioData
+{
+    self = [super init];
+    if (self) {
+        _uniqueID = [TCPCommentClipModel generateUUID];
+        _audioFileUrl = audioData;
+    }
+    return self;
+}
+
+- (void)saveInBackground
+{
+    NSData *audioData = [[NSFileManager defaultManager] contentsAtPath:[_audioFileUrl path]];
+    NSString *awsKey = [NSString stringWithFormat:@"comment_clip_%@", _uniqueID];
+    [TCPAwsAPI upload:audioData inBucket:@"pronounce" forKey:awsKey];
+}
 
 @end
